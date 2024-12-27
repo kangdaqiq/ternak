c7922d43365b5258e7f7d07812b7fc7593488e69
kangdaqiq <65300319+kangdaqiq@users.noreply.github.com>
2024-12-27 18:12:52 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -74,13 +74,13 @@ tmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $9" "substr ($
 cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
 cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
 cpu_usage+=" %"
-ISP=$(curl -s ipinfo.io/org?token=7578ac19afd785 | cut -d " " -f 2-10 )
-CITY=$(curl -s ipinfo.io/city?token=7578ac19afd785 )
-WKT=$(curl -s ipinfo.io/timezone?token=7578ac19afd785 )
+ISP=$(curl -s ipinfo.io/org?token=6b4c907900085a | cut -d " " -f 2-10 )
+CITY=$(curl -s ipinfo.io/city?token=6b4c907900085a )
+WKT=$(curl -s ipinfo.io/timezone?token=6b4c907900085a )
 DAY=$(date +%A)
 DATE=$(date +%m/%d/%Y)
 DATE2=$(date -R | cut -d " " -f -5)
-IPVPS=$(curl -s ipinfo.io/ip?token=7578ac19afd785 )
+IPVPS=$(curl -s ipinfo.io/ip?token=6b4c907900085a )
 cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
 cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
 freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
